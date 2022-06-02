93119e74f725edabb997908f8a41b92a6013b01f
jennasaikaly <jen.saikaly@gmail.com>
2022-06-01 21:00:06 -0400

    homepage routes setup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Plant, User, Log } = require('../models');
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+//NEED SEED
+// get all plants
+router.get('/', (req, res)=>{
+    Plant.findAll({
+        order: [['common_name', 'ASC']],
+        include: [
+            {
+                model: User,
+                attributes: ['email']
+            }
+        ]
+    })
+    .then(dbPlantData => {
+        // pass a single plant object into the homepage template
+        res.render('homepage', dbPlantData[0]);
+        
+        //NEED SEED?
+        // //loops over each object and serializes it
+        // const plants = dbPlantData.map(plant => plant.get({ plain: true }));
+        // res.render('homepage', dbPlantData[0].get({ plain: true }));
+      })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
