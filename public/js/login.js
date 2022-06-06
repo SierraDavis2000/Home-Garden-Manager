335efb70f49f519876bcf8d4684c7b08fc9ef9a7
Rosemarie <rosemarie.m.halim@gmail.com>
2022-06-05 20:39:48 -0400

    adding changes

--- public/js/login.js
+++ public/js/login.js
@@ -34,7 +34,7 @@ async function signupFormHandler(event){
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
