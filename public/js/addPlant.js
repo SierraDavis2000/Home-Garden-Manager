fdf8735d23c21c0193b57ace84b6ffed12033ad6
Rosemarie <rosemarie.m.halim@gmail.com>
2022-06-09 18:44:26 -0400

    fixing addplant feature

--- public/js/addPlant.js
+++ public/js/addPlant.js
@@ -2,13 +2,29 @@ const newFormHandler = async function(event) {
   event.preventDefault();
 
   const postTitle = document.querySelector('input[name="post-title"]').value;
-  const postContent = document.querySelector('textarea[name="post-body"]').value;
+  const scContent = document.querySelector('input[name="sc-content"]').value;
+  const wsContent = document.querySelector('input[name="ws-content"]').value;
+  const stContent = document.querySelector('input[name="st-content"]').value;
+  const lrContent = document.querySelector('input[name="lr-content"]').value;
+  const frContent = document.querySelector('input[name="fr-content"]').value;
+  const srContent = document.querySelector('input[name="sr-content"]').value;
+  const ioContent = document.querySelector('input[name="io-content"]').value;
+  const piContent = document.querySelector('input[name="pi-content"]').value;
+  const taContent = document.querySelector('input[name="ta-content"]').value;
 
   await fetch(`/api/post`, {
     method: 'POST',
     body: JSON.stringify({
       postTitle,
-      postContent,
+      scContent,
+      wsContent,
+      stContent,
+      lrContent,
+      frContent,
+      srContent,
+      ioContent,
+      piContent,
+      taContent
     }),
     headers: { 'Content-Type': 'application/json' },
   });
